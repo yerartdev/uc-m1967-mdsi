d317935a334ffac724f2872b7a5dbfd27081506e
yerartdev <gdmiguel@gmail.com>
2018-12-09 09:31:12 +0100

    SQL Práctica 2: creación db y fichero de entrega sql (ejercicios a completar)

---
+++ d9/de-miguel-gonzalez-gerardo.sql
@@ -0,0 +1,28 @@
+--------------------------------------------------------------------------------
+-- Alumno: Gerardo de Miguel González                                         --
+--------------------------------------------------------------------------------
+-- Práctica 2 (ejercicio de seguimiento)                                      --
+-- Consultas sobre una base de datos                                          --
+--------------------------------------------------------------------------------
+
+-- 1. Mostrar los datos de los pedidos realizados entre octubre y noviembre de 2018 (0.5 ptos)
+
+-- 2. Devolver el id, nombre, apellido1, apellido2, fecha de alta y fecha de baja de todos los miembros del personal que no estén de baja, ordenados descendentemente por fecha de alta y ascendentemente por nombre (0.75 pto, 0.25 ptos adicionales si la consulta se realiza con el nombre y apellidos concatenados).
+
+-- 3. Retornar los datos de todos los clientes cuyo nombre de calle comience por G o J y que además tengan observaciones (1 pto).
+
+-- 4. Devolver el id e importe de las pizzas junto con el id y descripción de todos sus ingredientes, siempre que el importe de estas pizzas sea mayor de 3 (1 pto).
+
+-- 5. Mostrar los datos de todas las pizzas que no hayan sido nunca pedidas, ordenados por id ascendentemente (1 pto).
+
+-- 6. Devolver los datos de las bases, junto con los datos de las pizzas en las que están presentes, incluyendo los datos de las bases que no están en ninguna pizza (0.5 ptos)
+
+-- 7. Retornar los datos de los pedidos realizados por el cliente con id 1, junto con los datos de sus líneas y de las pizzas pedidas, siempre que el precio unitario en la línea sea menor que el importe base de la pizza. (1.5 ptos)
+
+-- 8. Mostrar el id y nif de todos los usuarios, junto con el número total de pedidos realizados (0.75 pto, 0.25 ptos adicionales si sólo se devuelven los datos de los que hayan realizado más de un pedido).
+
+-- 9. Sumar 0.5 al importe base de todas las pizzas que contengan el ingrediente con id 1 (0.75 pto).
+
+-- 10. Eliminar las líneas de los pedidos anteriores a 2018 (0.75 pto).
+
+-- 11. BONUS para el 10: Realizar una consulta que devuelva el número de pizzas totales pedidas por cada cliente. En la consulta deberán aparecer el id y nif de los clientes, además de su nombre y apellidos concatenados (1 pto).
